c188cf1d9b5711b9569c1f5f344c73014b569b6c
cjfontaine27 <cjfontaine11@gmail.com>
2024-10-20 21:24:09 -0400

    changes

--- un-sdg.js
+++ un-sdg.js
@@ -38,8 +38,15 @@ export class unSdg extends DDDSuper(I18NMixin(LitElement)) {
   // Lit reactive properties
   static get properties() {
     return {
-      ...super.properties,
       title: { type: String },
+      goal: { type: String, reflect: true },
+      width: { type: String },
+      height: { type: String },
+      label: { type: String },
+      loading: { type: String, reflect: true },
+      fetchPriority: { type: String, reflect: true },
+      color: { type: String },
+      colorOnly: { type: Boolean },
     };
   }
 
@@ -63,13 +70,102 @@ export class unSdg extends DDDSuper(I18NMixin(LitElement)) {
     `];
   }
 
+  updated(changedProperties) {
+    // When a 'goal' is set, update the alt text
+    if (changedProperties.has("goal")) {
+      this.updateAlt();
+    }
+  }
+
+  updateAlt() {
+    const goal = this.getAttribute("goal");
+    switch (goal) {
+      case "circle":
+        this.label = "Sustainable developments logo";
+        break;
+      case "all":
+        this.label =
+          "Goal 1: No poverty, Goal 2: Zero hunger, Goal 3: Good health and well-being, Goal 4: Quality education, Goal 5: Gender equality, Goal 6: Clean water and sanitation, Goal 7: Affordable and clean energy, Goal 8: Decent work and economic growth, Goal 9: Industry, innovation and infrastructure, Goal 10: Reduced inequalities, Goal 11: Sustainable cities and communities, Goal 12: Responsible consumption and production, Goal 13: Climate action, Goal 14: Life below water, Goal 15: Life on land, Goal 16: Peace, justice and strong institutions, Goal 17: Partnerships for the goals";
+
+        break;
+      case "1":
+        this.label = "Goal 1: No poverty";
+        this.color = "rgb(235, 28, 44)";
+        break;
+      case "2":
+        this.label = "Goal 2: Zero hunger";
+        this.color = "rgb(210, 160, 42)";
+        break;
+      case "3":
+        this.label = "Goal 3: Good health and well-being";
+        this.color = "rgb(44, 155, 72)";
+        break;
+      case "4":
+        this.label = "Goal 4: Quality education";
+        this.color = "rgb(194, 31, 51)";
+        break;
+      case "5":
+        this.label = "Goal 5: Gender equality";
+        this.color = "rgb(239, 64, 42)";
+        break;
+      case "6":
+        this.label = "Goal 6: Clean water and sanitation";
+        this.color = "rgb(0, 173, 216)";
+        break;
+      case "7":
+        this.label = "Goal 7: Affordable and clean energy";
+        this.color = "rgb(253, 183, 19)";
+        break;
+      case "8":
+        this.label = "Goal 8: Decent work and economic growth";
+        this.color = "rgb(143, 23, 55)";
+        break;
+      case "9":
+        this.label = "Goal 9: Industry, innovation and infrastructure";
+        this.color = "rgb(243, 109, 36)";
+        break;
+      case "10":
+        this.label = "Goal 10: Reduced inequalities";
+        this.color = "rgb(224, 21, 131)";
+        break;
+      case "11":
+        this.label = "Goal 11: Sustainable cities and communities";
+        this.color = "rgb(249, 157, 37)";
+        break;
+      case "12":
+        this.label = "Goal 12: Responsible consumption and production";
+        this.color = "rgb(207, 141, 42)";
+        break;
+      case "13":
+        this.label = "Goal 13: Climate action";
+        this.color = "rgb(72, 119, 61)";
+        break;
+      case "14":
+        this.label = "Goal 14: Life below water";
+        this.color = "rgb(0, 125, 187)";
+        break;
+      case "15":
+        this.label = "Goal 15: Life on land";
+        this.color = "rgb(63, 175, 73)";
+        break;
+      case "16":
+        this.label = "Goal 16: Peace, justice and strong institutions";
+        this.color = "rgb(1, 85, 138)";
+        break;
+      case "17":
+        this.label = "Goal 17: Partnerships for the goals";
+        this.color = "rgb(25, 54, 103)";
+        break;
+    }
+  }
+
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+    <div class="wrapper">
+      <h3><span>${this.t.title}:</span> ${this.title}</h3>
+      <slot></slot>
+    </div>`;
   }
 
   /**
